4d96c7ec81e61b2384fc781352fe76d5abb77932
simmco <m.simmelbauer@gmail.com>
2016-08-29 16:52:11 +0200

    bugfix

--- index.js
+++ index.js
@@ -1,51 +1,19 @@
 window.onload = function() {
 
-  $('#sessionMinus').click(function() {
-    var min = parseInt(document.getElementById("sessionT").innerHTML);
-    if (min >= 2) {
-      document.getElementById("sessionT").innerHTML = min - 1;
-      document.getElementById("timer").innerHTML = document.getElementById("sessionT").innerHTML + ":" + "00";
-      clearInterval(intervalId);
-      running = false;
-    }
-  })
-
-  $('#sessionPlus').click(function() {
-    var min = parseInt(document.getElementById("sessionT").innerHTML);
-    document.getElementById("sessionT").innerHTML = min + 1;
-    document.getElementById("timer").innerHTML = document.getElementById("sessionT").innerHTML + ":" + "00";
-    clearInterval(intervalId);
-    running = false;
-  })
-
-  $('#breakMinus').click(function() {
-    var min = parseInt(document.getElementById("breakT").innerHTML);
-    if (min >= 2) {
-      document.getElementById("breakT").innerHTML = min - 1;
-      clearInterval(intervalId);
-      running = false;
-    }
-  })
-
-  $('#breakPlus').click(function() {
-    var min = parseInt(document.getElementById("breakT").innerHTML);
-    document.getElementById("breakT").innerHTML = min + 1;
-    clearInterval(intervalId);
-    running = false;
-  })
-
   //variable for setInterval
   var intervalId = 0;
   //variable to check if timer is running
   var running = false;
   //variable to check if it is break time
   var breakT = false;
+  var minS = parseInt(document.getElementById("sessionT").innerHTML);
+  var minT = parseInt(document.getElementById("breakT").innerHTML);
+  var sec = 59;
 
   $('.menu').click(function() {
-    var minS = parseInt(document.getElementById("sessionT").innerHTML);
-    var minT = parseInt(document.getElementById("breakT").innerHTML);
+
     // running or stop
-    var sec = 59;
+
     if (running === false) {
       // setInterval 1000
       intervalId = setInterval(function() {
@@ -86,4 +54,50 @@ window.onload = function() {
     }
 
   });
+
+  $('#sessionMinus').click(function() {
+    var min = parseInt(document.getElementById("sessionT").innerHTML);
+    if (min >= 2) {
+      document.getElementById("sessionT").innerHTML = min - 1;
+      document.getElementById("timer").innerHTML = document.getElementById("sessionT").innerHTML + ":" + "00";
+      minS = parseInt(document.getElementById("sessionT").innerHTML);
+      minT = parseInt(document.getElementById("breakT").innerHTML);
+      sec = 59;
+      clearInterval(intervalId);
+      running = false;
+    }
+  })
+
+  $('#sessionPlus').click(function() {
+    var min = parseInt(document.getElementById("sessionT").innerHTML);
+    document.getElementById("sessionT").innerHTML = min + 1;
+    document.getElementById("timer").innerHTML = document.getElementById("sessionT").innerHTML + ":" + "00";
+    minS = parseInt(document.getElementById("sessionT").innerHTML);
+    minT = parseInt(document.getElementById("breakT").innerHTML);
+    sec = 59;
+    clearInterval(intervalId);
+    running = false;
+  })
+
+  $('#breakMinus').click(function() {
+    var min = parseInt(document.getElementById("breakT").innerHTML);
+    if (min >= 2) {
+      document.getElementById("breakT").innerHTML = min - 1;
+      minS = parseInt(document.getElementById("sessionT").innerHTML);
+      minT = parseInt(document.getElementById("breakT").innerHTML);
+      sec = 59;
+      clearInterval(intervalId);
+      running = false;
+    }
+  })
+
+  $('#breakPlus').click(function() {
+    var min = parseInt(document.getElementById("breakT").innerHTML);
+    document.getElementById("breakT").innerHTML = min + 1;
+    minS = parseInt(document.getElementById("sessionT").innerHTML);
+    minT = parseInt(document.getElementById("breakT").innerHTML);
+    sec = 59;
+    clearInterval(intervalId);
+    running = false;
+  })
 }
