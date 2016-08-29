ba627897900e8bce1776940bd9c1d1c630b2be86
simmco <m.simmelbauer@gmail.com>
2016-08-29 11:31:19 +0200

    finished clock

--- index.js
+++ index.js
@@ -1,25 +1,89 @@
-window.onload = function(){
+window.onload = function() {
 
-$('.menu').click(function () {
-  
-  var min = 25;
-  var sec = 60;
-  
- 
-   
-  setInterval(function() {    
-    document.getElementById("timer").innerHTML = min + " : " + sec ;
-  sec--;
-    if(sec==0) {
-      min--;
-      sec=60;
-      if(min==0) {
-        min=25;
-      }
+  $('#sessionMinus').click(function() {
+    var min = parseInt(document.getElementById("sessionT").innerHTML);
+    if (min >= 2) {
+      document.getElementById("sessionT").innerHTML = min - 1;
+      document.getElementById("timer").innerHTML = document.getElementById("sessionT").innerHTML + ":" + "00";
+      clearInterval(intervalId);
+      running = false;
     }
-    
-  },1000);
-  
-});  
-  
+  })
+
+  $('#sessionPlus').click(function() {
+    var min = parseInt(document.getElementById("sessionT").innerHTML);
+    document.getElementById("sessionT").innerHTML = min + 1;
+    document.getElementById("timer").innerHTML = document.getElementById("sessionT").innerHTML + ":" + "00";
+    clearInterval(intervalId);
+    running = false;
+  })
+
+  $('#breakMinus').click(function() {
+    var min = parseInt(document.getElementById("breakT").innerHTML);
+    if (min >= 2) {
+      document.getElementById("breakT").innerHTML = min - 1;
+      clearInterval(intervalId);
+      running = false;
+    }
+  })
+
+  $('#breakPlus').click(function() {
+    var min = parseInt(document.getElementById("breakT").innerHTML);
+    document.getElementById("breakT").innerHTML = min + 1;
+    clearInterval(intervalId);
+    running = false;
+  })
+
+  //variable for setInterval
+  var intervalId = 0;
+  //variable to check if timer is running
+  var running = false;
+  //variable to check if it is break time
+  var breakT = false;
+
+  $('.menu').click(function() {
+    var minS = parseInt(document.getElementById("sessionT").innerHTML);
+    var minT = parseInt(document.getElementById("breakT").innerHTML);
+    // running or stop
+    var sec = 59;
+    if (running === false) {
+      // setInterval 1000
+      intervalId = setInterval(function() {
+        // is BreakTime running
+        if (breakT === false) {
+          console.log(minS);
+          document.getElementById("timer").innerHTML = minS - 1 + " : " + sec;
+          sec--;
+          if (sec == -1) {
+            minS--;
+            sec = 59;
+            if (minS == 0) {
+              minS = document.getElementById("sessionT").innerHTML;
+              document.getElementById("session").innerHTML = "Break";
+              breakT = true;
+            }
+          }
+        } else {
+          document.getElementById("timer").innerHTML = minT - 1 + " : " + sec;
+          sec--;
+          if (sec == -1) {
+            minT--;
+            sec = 59;
+            if (minT == 0) {
+              minT = document.getElementById("breakT").innerHTML;
+              document.getElementById("session").innerHTML = "Session";
+              breakT = false;
+
+            }
+          }
+
+        }
+      }, 1000);
+      running = true;
+    } else {
+      clearInterval(intervalId);
+      running = false;
+    }
+
+  });
 }
